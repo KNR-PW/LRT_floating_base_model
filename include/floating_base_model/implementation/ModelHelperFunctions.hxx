02627fd6fb629aececc784e1bed5828c591cf368
Bartek <bartlomiejk@vp.pl>
2025-02-22 14:34:42 +0100

    Added computeBaseBodyAcceleration

--- include/floating_base_model/implementation/ModelHelperFunctions.hxx
+++ include/floating_base_model/implementation/ModelHelperFunctions.hxx
@@ -17,10 +17,29 @@ namespace floating_base_model
       const Eigen::Matrix<SCALAR_T, 3, 3> cx = mass_inv * Mb.template block<3, 3>(3, 0);
       const Eigen::Matrix<SCALAR_T, 3, 3> Ic_inv = (Mb.template block<3, 3>(3, 3)  + Mb.template block<3, 3>(3, 0) * cx).inverse();
       const Eigen::Matrix<SCALAR_T, 3, 3> Ic_inv_cx = Ic_inv * cx;
-      Eigen::Matrix<SCALAR_T, 6, 6> Mb_inv = Eigen::Matrix<SCALAR_T, 6, 6>::Zero();
+      Eigen::Matrix<SCALAR_T, 6, 6> Mb_inv;
       Mb_inv << mass_inv * Eigen::Matrix<SCALAR_T, 3, 3>::Identity() - cx * Ic_inv_cx, cx * Ic_inv,
       - Ic_inv_cx, Ic_inv;
+      
       return Mb_inv;
     };
+
+    /******************************************************************************************************/
+    /******************************************************************************************************/
+    /******************************************************************************************************/
+    template <typename SCALAR_T>
+    Eigen::Matrix<SCALAR_T, 6, 1> computeBaseBodyAcceleration(const Eigen::Matrix<SCALAR_T, 6, 6>& Mb, const Eigen::Matrix<SCALAR_T, 6, 1>& tau)
+    {
+      const SCALAR_T mass_inv = SCALAR_T(1.0) / Mb(0, 0);
+      const Eigen::Matrix<SCALAR_T, 3, 3> cx = mass_inv * Mb.template block<3, 3>(3, 0);
+      const Eigen::Matrix<SCALAR_T, 3, 3> Ic_inv = (Mb.template block<3, 3>(3, 3)  + Mb.template block<3, 3>(3, 0) * cx).inverse();
+      
+      Eigen::Matrix<SCALAR_T, 6, 1> baseAcceleration;
+      baseAcceleration.template block<3,1>(3, 0).noalias() = Ic_inv * (tau.template block<3, 1>(3, 0) - cx * tau.template block<3, 1>(0, 0));
+      baseAcceleration.template block<3,1>(0, 0).noalias() = mass_inv * tau.template block<3, 1>(0, 0) + cx * baseAcceleration.template block<3,1>(3, 0);
+
+      return baseAcceleration;
+    };
+  
   };
 }; // namespace floating_base_model
